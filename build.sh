2138ccc78ba729091fc347917e20be04dbc04cff
Huevos <rowland@moralet.com>
2024-04-04 20:57:35 +0200

    Add a build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd meta
+version=$(grep Version control|cut -d " " -f 2)
+package=$(grep Package control|cut -d " " -f 2)
+mkdir -p usr/lib/enigma2/python/Plugins/SystemPlugins/M3UIPTV
+cp -r ../src ./usr/lib/enigma2/python/Plugins/SystemPlugins/M3UIPTV
+tar -cvzf data.tar.gz usr
+tar -cvzf control.tar.gz control
+
+rm -f ../${package}_${version}_all.ipk
+ar -r ../${package}_${version}_all.ipk debian-binary control.tar.gz data.tar.gz
+
+rm -fr control.tar.gz data.tar.gz usr
