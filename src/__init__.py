e7b5b8868208d8bf6228fb26c3aa5bc04bd29810
Huevos <rowland@moralet.com>
2024-04-04 21:34:29 +0200

    [__init__.py] add compatibility for translation

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import gettext
+
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+
+
+PluginLanguageDomain = "m3uiptv"
+PluginLanguagePath = "SystemPlugins/M3UIPTV/locale"
+
+
+def pluginlanguagedomain():
+	return PluginLanguageDomain
+
+
+def localeInit():
+	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+
+
+def _(txt):
+	if translated := gettext.dgettext(PluginLanguageDomain, txt):
+		return translated
+	else:
+		print("[" + PluginLanguageDomain + "] fallback to default translation for " + txt)
+		return gettext.gettext(txt)
+
+
+localeInit()
+language.addCallback(localeInit)
