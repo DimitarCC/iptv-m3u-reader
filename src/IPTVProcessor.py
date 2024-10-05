cd8abdcf65d35995700d3df45774f48bbc3bf4e1
Huevos <rowland@moralet.com>
2024-10-05 13:00:30 +0200

    Add tmdb by id

--- src/IPTVProcessor.py
+++ src/IPTVProcessor.py
@@ -128,7 +128,7 @@ class IPTVProcessor():
 				ext = movie["container_extension"]
 				id = movie["stream_id"]
 				url = "%s/movie/%s/%s/%s.%s" % (self.url, self.username, self.password, id, ext)
-				vod_item = VoDItem(url, name, id, self.movie_categories.get(movie.get("category_id")), movie.get("plot"))
+				vod_item = VoDItem(url, name, id, self, self.movie_categories.get(movie.get("category_id")), movie.get("plot"))
 				self.vod_movies.append(vod_item)
 
 	def processService(self, nref, iptvinfodata, callback=None):
