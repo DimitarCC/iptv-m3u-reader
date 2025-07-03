24d6bb8518f693023dab74b8c5b07e5000e1d6cf
Huevos <rowland@moralet.com>
2025-07-03 20:16:04 +0200

    [setup.py] pep8 update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 import setup_translate
 
 pkg = 'SystemPlugins.M3UIPTV'
-setup(name='enigma2-plugin-systemplugins-m3uiptv',
-       version='1.0',
-       author='DimitarCC',
-       description='IPTV m3u list dynamic reader and runner',
-       package_dir={pkg: 'src'},
-       packages=[pkg],
-       package_data={pkg:
-           ['*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo']},
-       cmdclass=setup_translate.cmdclass, # for translation
-      )
+setup(
+    name='enigma2-plugin-systemplugins-m3uiptv',
+    version='1.0',
+    author='DimitarCC',
+    description='IPTV m3u list dynamic reader and runner',
+    package_dir={pkg: 'src'},
+    packages=[pkg],
+    package_data={pkg: ['*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo']},
+    cmdclass=setup_translate.cmdclass,  # for translation
+    )
